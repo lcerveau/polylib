23ea8318aced9875670f6a718f843fd7b154509f
Laurent Cerveau <lcerveau@mmyneta.com>
2017-01-23 07:53:24 +0100

    Initial Commit

---
+++ polylib/polylib.h
@@ -0,0 +1,19 @@
+//
+//  polylib.h
+//  polylib
+//
+//  Created by Laurent Cerveau on 01/23/2017.
+//  Copyright © 2017 MMyneta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for polylib.
+FOUNDATION_EXPORT double polylibVersionNumber;
+
+//! Project version string for polylib.
+FOUNDATION_EXPORT const unsigned char polylibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <polylib/PublicHeader.h>
+
+
